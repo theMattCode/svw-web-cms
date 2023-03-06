eedb7a9ad66226ac640726b2cb021dcd1164e3be
Matthäus Mayer <7984982+theMattCode@users.noreply.github.com>
2023-03-06 11:28:46 +0100

    Prepare productive DB connection

--- strapi/config/database.ts
+++ strapi/config/database.ts
@@ -1,16 +1,35 @@
 import path from "path";
 
-export default ({ env }) => ({
-  connection: {
-    client: "sqlite",
-    connection: {
-      filename: path.join(
-        __dirname,
-        "..",
-        "..",
-        env("DATABASE_FILENAME", "database/dev-data.db")
-      ),
-    },
-    useNullAsDefault: true,
-  },
-});
+export default ({ env }) => {
+  const config = env("CONFIGURATION", "dev");
+  switch (config) {
+    case "dev":
+      return {
+        connection: {
+          client: "sqlite",
+          connection: {
+            filename: path.join(
+              __dirname,
+              "..",
+              "..",
+              env("DATABASE_FILENAME", "database/dev-data.db")
+            ),
+          },
+          useNullAsDefault: true,
+        },
+      };
+    case "prod":
+      return {
+        connection: {
+          client: "mysql",
+          connection: {
+            host: "localhost",
+            user: env("DB_USER"),
+            password: env("DB_PASSWORD"),
+            database: env("DB_NAME"),
+          },
+          useNullAsDefault: true,
+        },
+      };
+  }
+};
